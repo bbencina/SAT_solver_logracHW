7bb32ce01d593b3d521f67e937c5f0d6a4042689
bbencina <benjamin.bencina@gmail.com>
2020-03-11 23:04:12 +0100

    Fix NoneType error

--- code/parser.py
+++ code/parser.py
@@ -14,49 +14,34 @@ def select_lit(cnf):
         for lit in dis:
             return abs(lit)
 
-def dpll(cnf, assign):
-    print('Checking end condition...')
+def assign_lit(cnf, lit, val):
+    coef = 1 if val else -1
+    new_cnf = []
+    new_cnf = [dis for dis in cnf if coef*lit not in dis]
+    new_cnf = [dis.difference({-coef*lit}) for dis in new_cnf]
+    return new_cnf
+
+def dpll(cnf, assign={}):
     if len(cnf) == 0:
         return True, assign
     elif any([len(dis) == 0 for dis in cnf]):
-        return False, assign
-    print('Selecting new literal...')
+        return False, None
     lit = select_lit(cnf)
     # true branch
-    print('===TRUE===')
-    new_cnf = []
-    print(lit, 'True')
-    print('Constructing new cnf...')
-    new_cnf = [dis for dis in cnf if lit not in dis]
-    print('Step 1: ', new_cnf)
-    new_cnf = [dis.difference({-lit}) for dis in new_cnf]
-    print('Step 2: ', new_cnf)
-    print('Assign: ', assign)
-    sat, assign = dpll(new_cnf, {**assign, lit:True})
+    new_cnf = assign_lit(cnf, lit, True)
+    sat, vals = dpll(new_cnf, {**assign, lit:True})
     if sat:
-        return sat, assign
-    else:
-        del assign[lit]
+        return sat, vals
     # false branch
-    print('===FALSE===')
-    new_cnf = []
-    print(lit, 'False')
-    print('Constructing new cnf...')
-    new_cnf = [dis for dis in cnf if -lit not in dis]
-    print('Step 1: ', new_cnf)
-    new_cnf = [dis.difference({lit}) for dis in new_cnf]
-    print('Step 2: ', new_cnf)
-    print('Assign: ', assign)
-    sat, assign = dpll(new_cnf, {**assign, lit:False})
+    new_cnf = assign_lit(cnf, lit, False)
+    sat, vals = dpll(new_cnf, {**assign, lit:False})
     if sat:
-        return sat, assign
-    else:
-        del assign[lit]
+        return sat, vals
+    return False, None
 
 def main():
-    fname = r'..\test\basic.txt'
+    fname = r'..\..\Homework Files-20200311\sudoku_easy.txt'
     cnf = dimacs_to_cnf(fname)
-    print(cnf)
     print(dpll(cnf, {}))
 
 main()
