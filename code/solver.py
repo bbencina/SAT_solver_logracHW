0d3c9a78a91162dcd5ef3c4c9aa2f5f6f79710af
y5qtp4wd <62075841+y5qtp4wd@users.noreply.github.com>
2020-03-16 02:56:50 +0100

    hard now working, added internal checker to solver.py, added output file cli args

--- code/solver.py
+++ code/solver.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 import sys
-import os
+#import os
+
+from functools import reduce
 
 def parse_dimacs(dimacs_file):
     cnf = []
@@ -60,13 +62,13 @@ def dpll(cnf, assign={}):
         return False, None
 
     # unit propagation
-    new_cnf, new_assign = assign_unit_clauses(cnf)
-    new_assign = {**assign, **new_assign}
+    cnf, new_assign = assign_unit_clauses(cnf)
+    assign = {**assign, **new_assign}
 
-    if len(new_cnf) == 0:
-        return True, new_assign
+    if len(cnf) == 0:
+        return True, assign
 
-    if any([len(dis) == 0 for dis in new_cnf]):
+    if any([len(dis) == 0 for dis in cnf]):
         return False, None
 
 
@@ -74,47 +76,76 @@ def dpll(cnf, assign={}):
 
 
     # branching literal assignment
-    lit = select_lit(new_cnf)
+    lit = select_lit(cnf)
 
     # true branch
-    new_cnf = assign_lit(new_cnf, lit, True)
-    sat, vals = dpll(new_cnf, {**new_assign, lit:True})
+    new_cnf = assign_lit(cnf, lit, True)
+    sat, vals = dpll(new_cnf, {**assign, lit:True})
     if sat:
         return sat, vals
 
     # false branch
-    new_cnf = assign_lit(new_cnf, lit, False)
-    sat, vals = dpll(new_cnf, {**new_assign, lit:False})
+    new_cnf = assign_lit(cnf, lit, False)
+    sat, vals = dpll(new_cnf, {**assign, lit:False})
     if sat:
         return sat, vals
 
     return False, None
 
+
+def verify_solution(cnf, vals):
+
+    assign = lambda l : vals[l] if (l > 0) else not vals[-l]
+
+    for dis in cnf:
+        if not reduce((lambda x,y : x or y), map(assign, dis)):
+            return False
+
+    return True
+
+
 def main(argv):
 
-    if (len(argv) != 2):
-        print("Usage: {} <dimacs-file>".format(argv[0]))
+    if (len(argv) != 3):
+        print("Usage: {} <dimacs-file> <output-file>".format(argv[0]))
         return
 
     in_file_path = argv[1]
+    out_file_path = argv[2]
 
     with open(in_file_path, 'r') as file_handle:
-        print('Parsing input file...', end='')
+        #print('Parsing input file...', end='')
         cnf = parse_dimacs(file_handle)
-        print('Done.')
-        print('Checking satisfiability...', end='')
-        sat, vals = dpll(cnf)
-        print('Done.')
-        head, tail = os.path.split(in_file_path)
-        root_ext = os.path.splitext(tail)
-        o_file_name = root_ext[0] + '_solution' + root_ext[1]
-        if not sat:
-            make_solution_file(o_file_name, {})
-            print("Not satisfiable...wrote solution to " + o_file_name + ".")
-            return
 
-        make_solution_file(o_file_name, vals)
-        print('Satisfiable...wrote solution to ' + o_file_name + '.')
+        sat, vals = dpll(cnf)
+
+        if sat:
+            print("SAT")
+            check = verify_solution(cnf,vals)
+            if check:
+                print("Solution is valid.")
+            else:
+                print("Solution NOT valid!")
+
+            make_solution_file(out_file_path, vals)
+
+        else:
+            print("Not SAT")
+
+#        print('Done.')
+#        print('Checking satisfiability...', end='')
+#        sat, vals = dpll(cnf)
+#        print('Done.')
+#        head, tail = os.path.split(in_file_path)
+#        root_ext = os.path.splitext(tail)
+#        o_file_name = root_ext[0] + '_solution' + root_ext[1]
+#        if not sat:
+#            make_solution_file(o_file_name, {})
+#            print("Not satisfiable...wrote solution to " + o_file_name + ".")
+#            return
+#
+#        make_solution_file(o_file_name, vals)
+#        print('Satisfiable...wrote solution to ' + o_file_name + '.')
 
 if __name__ == "__main__":
     main(sys.argv)
