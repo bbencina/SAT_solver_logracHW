8dc0d0deb2b3f6ee6d0d6958be6c0c379b275db9
Petra Podlogar <petra.podlogar@hotmail.com>
2020-03-30 21:14:02 +0200

    wrote the comments

--- code/generator.py
+++ code/generator.py
@@ -1,13 +1,16 @@
 def to_dimacs(i, j, k):
+    # vertex i is colored with color j, k is the number of all colors
     return (i - 1) * k + j
 
 def gen(V, E, k):
     cnf = []
+    # each node of the graph has to be colored by one of k colors
     for i in V:
         vi = set()
         for j in range(1, k + 1):
             vi.add(to_dimacs(i, j, k + 1))
         cnf.append(vi)
+    # no two adjacent vertices have the same coloring
     for (i, j) in E:
         for l in range(1, k + 1):
             vij = set()
